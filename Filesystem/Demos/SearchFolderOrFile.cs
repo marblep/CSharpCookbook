7a4ffbbfddea2783dc0058ef61165b3634f46c07
marblep <marblep@hotmail.com>
2016-11-21 23:16:27 +0800

    Add Filesystem

---
+++ Filesystem/Demos/SearchFolderOrFile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSharpCookbook.Src.Filesystem.Utils;
+
+namespace CSharpCookbook.Src.Filesystem.Demos
+{
+    class SearchFolderOrFile : BaseApp
+    {
+        public override int GetPriority() { return 6; }
+
+        public override void Run()
+        {
+            FilesystemUtils.DisplayFilesAndSubDirectories(@"E:\Others");
+            //FilesystemUtils.DisplayFilesAndSubDirectories(@"D:\Books\马天心");
+            Console.WriteLine("");
+
+            FilesystemUtils.DisplaySubDirectories(@"E:\Others");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Filesystem/Utils/FilesystemUtils.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CSharpCookbook.Src.Filesystem.Utils
+{
+    class FilesystemUtils
+    {
+        public static void DisplayFilesAndSubDirectories(string path)
+        {
+            if (string.IsNullOrWhiteSpace(path))
+                throw new ArgumentNullException(nameof(path));
+            string[] items = Directory.GetFileSystemEntries(path);
+            Array.ForEach(items, item =>
+            {
+                Console.WriteLine(item);
+            });
+        }
+
+        public static void DisplaySubDirectories(string path)
+        {
+            if (string.IsNullOrWhiteSpace(path))
+                throw new ArgumentNullException(nameof(path));
+            string[] items = Directory.GetDirectories(path);
+            Array.ForEach(items, item =>
+            {
+                Console.WriteLine(item);
+            });
+        }
+    }
+}
